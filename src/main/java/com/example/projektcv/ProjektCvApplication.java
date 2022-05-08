8810081945a873d2075d267654425d070fd609aa
Guthak <tomekomen1@gmail.com>
2022-05-08 13:46:06 +0200

    Initial commit

---
+++ src/main/java/com/example/projektcv/ProjektCvApplication.java
@@ -0,0 +1,13 @@
+package com.example.projektcv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjektCvApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjektCvApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/projektcv/ProjektCvApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.projektcv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjektCvApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
